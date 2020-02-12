ced5e49bec01b940c156d6edb07e8df12826d915
Bryan Housel <bryan@mapbox.com>
2020-02-12 16:47:39 -0500

    Tell Greenkeeper to ignore turf dependencies

--- dist/index.js
+++ dist/index.js
@@ -1188,11 +1188,11 @@
 	};
 
 	function toRadians(angleInDegrees) {
-	  return angleInDegrees * Math.PI / 180;
+	  return (angleInDegrees * Math.PI) / 180;
 	}
 
 	function toDegrees(angleInRadians) {
-	  return angleInRadians * 180 / Math.PI;
+	  return (angleInRadians * 180) / Math.PI;
 	}
 
 	function offset(c1, distance, bearing) {
@@ -1201,40 +1201,105 @@
 	  var dByR = distance / 6378137; // distance divided by 6378137 (radius of the earth) wgs84
 	  var lat = Math.asin(
 	    Math.sin(lat1) * Math.cos(dByR) +
-	    Math.cos(lat1) * Math.sin(dByR) * Math.cos(bearing));
-	  var lon = lon1 + Math.atan2(
+	      Math.cos(lat1) * Math.sin(dByR) * Math.cos(bearing)
+	  );
+	  var lon =
+	    lon1 +
+	    Math.atan2(
 	      Math.sin(bearing) * Math.sin(dByR) * Math.cos(lat1),
-	      Math.cos(dByR) - Math.sin(lat1) * Math.sin(lat));
+	      Math.cos(dByR) - Math.sin(lat1) * Math.sin(lat)
+	    );
 	  return [toDegrees(lon), toDegrees(lat)];
+	}
+
+	function validateCenter(center) {
+	  const validCenterLengths = [2, 3];
+	  if (!Array.isArray(center) || !validCenterLengths.includes(center.length)) {
+	    throw new Error("ERROR! Center has to be an array of length two or three");
+	  }
+	  const [lng, lat] = center;
+	  if (typeof lng !== "number" || typeof lat !== "number") {
+	    throw new Error(
+	      `ERROR! Longitude and Latitude has to be numbers but where ${typeof lng} and ${typeof lat}`
+	    );
+	  }
+	  if (lng > 180 || lng < -180) {
+	    throw new Error(
+	      `ERROR! Longitude has to be between -180 and 180 but was ${lng}`
+	    );
+	  }
+
+	  if (lat > 90 || lat < -90) {
+	    throw new Error(
+	      `ERROR! Latitude has to be between -90 and 90 but was ${lat}`
+	    );
+	  }
+	}
+
+	function validateRadius(radius) {
+	  if (typeof radius !== "number") {
+	    throw new Error(
+	      `ERROR! Radius has to be a positive number but was: ${typeof radius}`
+	    );
+	  }
+
+	  if (radius <= 0) {
+	    throw new Error(
+	      `ERROR! Radius has to be a positive number but was: ${radius}`
+	    );
+	  }
+	}
+
+	function validateNumberOfSegments(numberOfSegments) {
+	  if (typeof numberOfSegments !== "number" && numberOfSegments !== undefined) {
+	    throw new Error(
+	      `ERROR! Number of segments has to be a number but was: ${typeof numberOfSegments}`
+	    );
+	  }
+
+	  if (numberOfSegments < 3) {
+	    throw new Error(
+	      `ERROR! Number of segments has to be at least 3 but was: ${numberOfSegments}`
+	    );
+	  }
+	}
+
+	function validateInput({ center, radius, numberOfSegments }) {
+	  validateCenter(center);
+	  validateRadius(radius);
+	  validateNumberOfSegments(numberOfSegments);
 	}
 
 	var circleToPolygon = function circleToPolygon(center, radius, numberOfSegments) {
 	  var n = numberOfSegments ? numberOfSegments : 32;
-	  var flatCoordinates = [];
+
+	  // validateInput() throws error on invalid input and do nothing on valid input
+	  validateInput({ center, radius, numberOfSegments });
+
 	  var coordinates = [];
 	  for (var i = 0; i < n; ++i) {
-	    flatCoordinates.push.apply(flatCoordinates, offset(center, radius, 2 * Math.PI * i / n));
+	    coordinates.push(offset(center, radius, (2 * Math.PI * -i) / n));
 	  }
-	  flatCoordinates.push(flatCoordinates[0], flatCoordinates[1]);
-
-	  for (var i = 0, j = 0; j < flatCoordinates.length; j += 2) {
-	    coordinates[i++] = flatCoordinates.slice(j, j + 2);
-	  }
+	  coordinates.push(coordinates[0]);
 
 	  return {
-	    type: 'Polygon',
-	    coordinates: [coordinates.reverse()]
+	    type: "Polygon",
+	    coordinates: [coordinates]
 	  };
 	};
 
 	var geojsonPrecision = createCommonjsModule(function (module) {
 	(function() {
 
-	  function parse(t, precision) {
+	  function parse(t, coordinatePrecision, extrasPrecision) {
 
 	    function point(p) {
-	      return p.map(function(e) {
-	        return 1 * e.toFixed(precision);
+	      return p.map(function(e, index) {
+	        if (index < 2) {
+	            return 1 * e.toFixed(coordinatePrecision);
+	        } else {
+	            return 1 * e.toFixed(extrasPrecision);
+	        }
 	      });
 	    }
 
